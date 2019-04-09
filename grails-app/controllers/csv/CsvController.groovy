f55ec1c448c44421732d7ccf82e5fcae259bba79
Michael Vandermillen <michael_vandermillen@harvard.edu>
2019-04-09 12:48:01 -0400

    war compiled under java 8; reduce pagination from 100 to 10

--- grails-app/controllers/csv/CsvController.groovy
+++ grails-app/controllers/csv/CsvController.groovy
@@ -28,7 +28,7 @@ class CsvController {
         render(text: "No results found for source: " + source + ", setSpec: " + setSpec, contentType: "text/plain", encoding: "UTF-8")
         return
       }
-      int numPages = (Integer.parseInt(numFound) / 100) + 1
+      int numPages = (Integer.parseInt(numFound) / 10) + 1
       def xslName = csvService.getXslDir() + "/" + lowersource + '.xsl'
       def xslCsvName = csvService.getXslDir() + "/" + "xml2csv" + '.xsl'
       def csvText = ""
@@ -37,7 +37,7 @@ class CsvController {
         if( i == 0)
           startNum = 0
         else
-          startNum = (i * 100)
+          startNum = (i * 10)
         def xmlUrlWPag = xmlUrl + "&start=" + startNum + "&limit=100"
         //println "xmlUrlWPag: " + xmlUrlWPag
         def xml = xmlUrlWPag.toURL().newReader('utf-8')
